a12b3b473277c93e6d2831804011442327f61593
Pavel Kuvshinchikov <kuvshinchikov7127@mail.ru>
2022-09-04 21:46:13 +0300

    Работа над DataManager и Ролями для приложения

--- FamilyFinance/Model/DataManager.cs
+++ FamilyFinance/Model/DataManager.cs
@@ -1,7 +1,10 @@
-﻿using FamilyFinance.WebApi.Service;
+﻿using FamilyFinance.Core.Extensions;
+using FamilyFinance.WebApi.Service;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +21,101 @@ namespace FamilyFinance.Model
 
         public static string CurrentDataBase { get; set; }
 
+        public static Role UserRole { get; set; } = Role.Admin;
+
         public static User CurrentUser { get; set; } = new User();
+
+        #region DataProperties
+        //Категории доходов
+        private static ObservableCollection<Category> _incomeСategories = new();
+        public static ObservableCollection<Category> IncomeСategories
+        {
+            get
+            {
+                return _incomeСategories;
+            }
+            set
+            {
+                _incomeСategories.Clear();
+                _incomeСategories.AddRange(value);
+            }
+        }
+
+        //Категории расходов
+        private static ObservableCollection<CategoryExpense> _expensesСategories = new();
+        public static ObservableCollection<CategoryExpense> ExpensesСategories
+        {
+            get
+            {
+                return _expensesСategories;
+            }
+            set
+            {
+                _expensesСategories.Clear();
+                _expensesСategories.AddRange(value);
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Заполняет свойства DataManager данными из сервера
+        /// </summary>
+        public static event EventHandler BeforeUpdate;
+        public static event EventHandler AfterUpdate;
+
+        public static async Task<bool> UpdateFromServer()
+        {
+            BeforeUpdate?.Invoke(typeof(DataManager), EventArgs.Empty);
+
+            Func<Client, Task> manipulationDataMethod = async (client) =>
+            {
+                // Загрузка данных
+                var incomeСategories = client.ApiCategoryGetAllByUserIdAsync(CurrentUser.Id); //загрузка категорий доходов
+                var expensesCategories = client.ApiCategoryExpenseGetAllByUserIdAsync(CurrentUser.Id); //загрузка категорий расходов
+
+                // Загрузка данных
+                await Task.WhenAll(
+                    incomeСategories,
+                    expensesCategories);
+
+                // Устанавливаем значения свойствам
+                IncomeСategories = new(incomeСategories.Result.ToList());
+                ExpensesСategories = new(expensesCategories.Result.ToList());
+            };
+
+            var resultStatus = await ClientManager.ManipulatonData(manipulationDataMethod);
+
+            if (resultStatus == HttpStatusCode.OK)
+            {
+                AfterUpdate?.Invoke(typeof(DataManager), EventArgs.Empty);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static async Task UpdateCategories()
+        {
+            Func<Client, Task> manipulationDataMethod = async (client) =>
+            {
+                var incomeСategories = await client.ApiCategoryGetAllByUserIdAsync(CurrentUser.Id); //загрузка категорий доходов
+                // Устанавливаем значения свойствам
+                IncomeСategories = new(incomeСategories.ToList());
+            };
+            await ClientManager.ManipulatonData(manipulationDataMethod);
+        }
+
+        public static async Task UpdateCategoriesExpenses()
+        {
+            Func<Client, Task> manipulationDataMethod = async (client) =>
+            {
+                var expensesСategories = await client.ApiCategoryExpenseGetAllByUserIdAsync(CurrentUser.Id); //загрузка категорий расходов
+                // Устанавливаем значения свойствам
+                ExpensesСategories = new(expensesСategories.ToList());
+            };
+            await ClientManager.ManipulatonData(manipulationDataMethod);
+        }
     }
 }
---
+++ FamilyFinance/Model/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyFinance.Model
+{
+    //Роли пользователей
+   public enum Role
+    {
+        Admin = 1,
+        Moderator = 2,
+        User = 3,
+        Guest = 4
+    }
+}
