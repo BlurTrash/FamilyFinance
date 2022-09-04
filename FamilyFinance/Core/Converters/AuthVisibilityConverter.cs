676dafd57bb6886ee49767933d8858d156b85020
Pavel Kuvshinchikov <kuvshinchikov7127@mail.ru>
2022-09-04 21:44:42 +0300

    Конвертеры для видимости по ролям

---
+++ FamilyFinance/Core/Converters/AuthVisibilityConverter.cs
@@ -0,0 +1,50 @@
+﻿using FamilyFinance.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FamilyFinance.Core.Converters
+{
+    public class AuthVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Role)
+            {
+                var role = (Role)value;
+
+                return role == Role.Guest ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AuthEnabledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Role)
+            {
+                var role = (Role)value;
+
+                return role == Role.Guest ? false : true;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FamilyFinance/Core/Converters/NonAuthVisibilityConverter.cs
@@ -0,0 +1,50 @@
+﻿using FamilyFinance.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FamilyFinance.Core.Converters
+{
+    public class NonAuthVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Role)
+            {
+                var role = (Role)value;
+
+                return role == Role.Guest ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NonAuthEnabledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Role)
+            {
+                var role = (Role)value;
+
+                return role == Role.Guest ? true : false;
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
