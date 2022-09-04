d6cd745ff847f332ed5188397807d985d64c3514
Pavel Kuvshinchikov <kuvshinchikov7127@mail.ru>
2022-09-04 21:43:32 +0300

    Основная команда

---
+++ FamilyFinance/Core/Commands/RelayCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FamilyFinance.Core.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private Action execute;
+        private Func<bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private Action<T> execute;
+        private Func<T, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+    }
+}
