062c50bc15a709a30a52605d8729d965964fbe05
Pavel Kuvshinchikov <kuvshinchikov7127@mail.ru>
2022-09-04 21:45:31 +0300

    Расширение для ObservableCollection

---
+++ FamilyFinance/Core/Extensions/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyFinance.Core.Extensions
+{
+    public static class Extensions
+    {
+        public static void AddRange<T>(this ObservableCollection<T> collection, ICollection<T> range)
+        {
+            foreach (T item in range)
+            {
+                collection.Add(item);
+            }
+        }
+    }
+}
