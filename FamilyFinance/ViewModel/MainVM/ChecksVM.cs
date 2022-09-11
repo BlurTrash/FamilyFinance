6ea8cc4f0041539122c0ca61105d32664f07552e
Pavel Kuvshinchikov <kuvshinchikov7127@mail.ru>
2022-09-11 15:27:29 +0300

    Добавление VM для счетов

---
+++ FamilyFinance/ViewModel/MainVM/ChecksVM.cs
@@ -0,0 +1,26 @@
+﻿using FamilyFinance.Model;
+using FamilyFinance.WebApi.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FamilyFinance.ViewModel.MainVM
+{
+    public class ChecksVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ChecksVM()
+        {
+
+        }
+
+        public Role UserRole { get { return DataManager.UserRole; } }
+    }
+}
