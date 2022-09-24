8ed888fe63ac94488030555d9e56b4796af4ef5f
Pavel Kuvshinchikov <kuvshinchikov7127@mail.ru>
2022-09-25 00:04:14 +0300

    TrueFalse-YesNo converter

---
+++ FamilyFinance/Core/Converters/TrueFalseConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FamilyFinance.Core.Converters
+{
+    public class TrueFalseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                bool result = (bool)value;
+
+                return result ? "Да" : "Нет";
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
